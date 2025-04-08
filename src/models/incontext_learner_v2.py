1f68271427f484db9c6e2f4a49d6ad29ea93f806
Hrayr Harutyunyan <harhro@gmail.com>
2025-04-08 16:29:37 +0000

    Adapt our GPTJModel to the new version of transformers

--- src/models/incontext_learner_v2.py
+++ src/models/incontext_learner_v2.py
@@ -16,6 +16,7 @@ from pytorch_lightning import LightningModule
 import torch
 import torch.nn as nn
 import torchmetrics
+import transformers
 
 from src.utils.custom_metrics import MinorityMajorityAccuracy, GroupAccuracy, WorstGroupAccuracy
 
@@ -25,6 +26,7 @@ torch.backends.cudnn.benchmark = True
 
 class GPTJModelV2(GPTJModel):
     def __init__(self, config):
+        assert transformers.__version__ == '4.48.0'
         super().__init__(config)
         for param in self.wte.parameters():
             param.requires_grad = False
@@ -32,143 +34,68 @@ class GPTJModelV2(GPTJModel):
     @torch.compile(fullgraph=True)
     def forward(
         self,
-        input_ids: Optional[torch.LongTensor] = None,
-        past_key_values: Optional[Tuple[Tuple[torch.Tensor]]] = None,
-        query_indices: Optional[torch.FloatTensor] = None,
-        token_type_ids: Optional[torch.LongTensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        head_mask: Optional[torch.FloatTensor] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
-        use_cache: Optional[bool] = None,
+        query_indices: Optional[torch.FloatTensor] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
+        **kwargs,
     ) -> Union[Tuple, BaseModelOutputWithPast]:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
-        use_cache = use_cache if use_cache is not None else self.config.use_cache
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        if input_ids is not None and inputs_embeds is not None:
-            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
-        elif input_ids is not None:
-            # self.warn_if_padding_and_no_attention_mask(input_ids, attention_mask)
-            input_shape = input_ids.size()
-            input_ids = input_ids.view(-1, input_shape[-1])
-            batch_size = input_ids.shape[0]
-        elif inputs_embeds is not None:
-            input_shape = inputs_embeds.size()[:-1]
-            batch_size = inputs_embeds.shape[0]
-        else:
-            raise ValueError("You have to specify either input_ids or inputs_embeds")
+        batch_size = inputs_embeds.shape[0]
+        seq_length = inputs_embeds.shape[1]
 
-        device = input_ids.device if input_ids is not None else inputs_embeds.device
-
-        if token_type_ids is not None:
-            token_type_ids = token_type_ids.view(-1, input_shape[-1])
-
-        assert past_key_values is None
-        past_key_values = tuple([None] * len(self.h))
-
-        # Constructing positions ids
-        assert position_ids is None
+        # Construct positions ids
         assert query_indices is not None
-        seq_len = input_shape[-1]
-        position_ids = torch.arange(0, seq_len, dtype=torch.long, device=device)
+        position_ids = torch.arange(0, seq_length, dtype=torch.long, device=inputs_embeds.device)
         query_indices = query_indices.to(dtype=torch.long)
-        query_mask = torch.zeros(seq_len + 1, dtype=torch.long)
+        query_mask = torch.zeros(seq_length + 1, dtype=torch.long)
         query_mask[1 + query_indices] = 1
-        position_ids -= torch.cumsum(query_mask, dim=0).to(device)[:seq_len]
-        position_ids = position_ids.unsqueeze(0)
+        position_ids -= torch.cumsum(query_mask, dim=0).to(inputs_embeds.device)[:seq_length]
+        position_ids = position_ids.unsqueeze(0).repeat((batch_size, 1))  # [batch_size, seq_length]
 
-        # Constructing attention_mask tensor of shape [batch_size, num_heads, seq_len, seq_len],
-        # which is going to be added after masking out non-causal pairs. Therefore, we just need
-        # to prepare the part where we disallow any token to attend to any query token, unless it
-        # is a query token attending to itself.
-        if batch_size <= 0:
-            raise ValueError("batch_size has to be defined and > 0")
-        attention_mask = torch.zeros((seq_len, seq_len),
-                                     dtype=self.dtype,
-                                     device=device)
-        attention_mask[:, query_indices] = torch.finfo(self.dtype).min / 2
-        attention_mask.fill_diagonal_(0)
-        attention_mask.tril_()
-        attention_mask = attention_mask.reshape((1, 1, seq_len, seq_len))#.tile((batch_size, 1, 1, 1))
-
-        # Prepare head mask if needed
-        # 1.0 in head_mask indicate we keep the head
-        # attention_probs has shape bsz x num_attention_heads x N x N
-        # head_mask has shape n_layer x batch x num_attention_heads x N x N
-        head_mask = self.get_head_mask(head_mask, self.config.n_layer)
-
-        if inputs_embeds is None:
-            inputs_embeds = self.wte(input_ids)
+        # Construct causal mask
+        causal_mask = self._prepare_4d_attention_mask(
+            sequence_length=seq_length,
+            dtype=inputs_embeds.dtype,
+            device=inputs_embeds.device,
+            batch_size=batch_size,
+            query_indices=query_indices,
+        )
 
         hidden_states = inputs_embeds
-
-        if token_type_ids is not None:
-            token_type_embeds = self.wte(token_type_ids)
-            hidden_states = hidden_states + token_type_embeds
-
         hidden_states = self.drop(hidden_states)
+        output_shape = (-1, seq_length, hidden_states.size(-1))
 
-        output_shape = (-1,) + input_shape[1:] + (hidden_states.size(-1),)
-
-        if self.gradient_checkpointing and self.training:
-            if use_cache:
-                # logger.warning_once(
-                #     "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                # )
-                use_cache = False
-
-        presents = () if use_cache else None
         all_self_attentions = () if output_attentions else None
         all_hidden_states = () if output_hidden_states else None
-        for i, (block, layer_past) in enumerate(zip(self.h, past_key_values)):
+        for i, block in enumerate(self.h):
             # Model parallel
             if self.model_parallel:
                 torch.cuda.set_device(hidden_states.device)
-                # Ensure layer_past is on same device as hidden_states (might not be correct)
-                if layer_past is not None:
-                    layer_past = tuple(past_state.to(hidden_states.device) for past_state in layer_past)
+
                 # Ensure that attention_mask is always on the same device as hidden_states
-                if attention_mask is not None:
-                    attention_mask = attention_mask.to(hidden_states.device)
-                if isinstance(head_mask, torch.Tensor):
-                    head_mask = head_mask.to(hidden_states.device)
+                if causal_mask is not None:
+                    causal_mask = causal_mask.to(hidden_states.device)
             if output_hidden_states:
                 all_hidden_states = all_hidden_states + (hidden_states,)
 
-            if self.gradient_checkpointing and self.training:
-                outputs = self._gradient_checkpointing_func(
-                    block.__call__,
-                    hidden_states,
-                    None,
-                    attention_mask,
-                    position_ids,
-                    head_mask[i],
-                    use_cache,
-                    output_attentions,
-                )
-            else:
-                outputs = block(
-                    hidden_states=hidden_states,
-                    layer_past=layer_past,
-                    attention_mask=attention_mask,
-                    position_ids=position_ids,
-                    head_mask=head_mask[i],
-                    use_cache=use_cache,
-                    output_attentions=output_attentions,
-                )
+            outputs = block(
+                hidden_states=hidden_states,
+                attention_mask=causal_mask,
+                position_ids=position_ids,
+                output_attentions=output_attentions,
+            )
 
             hidden_states = outputs[0]
-            if use_cache is True:
-                presents = presents + (outputs[1],)
 
             if output_attentions:
-                all_self_attentions = all_self_attentions + (outputs[2 if use_cache else 1],)
+                all_self_attentions = all_self_attentions + (outputs[1],)
 
             # Model Parallel: If it's the last layer for that device, put things on the next device
             if self.model_parallel:
@@ -185,14 +112,63 @@ class GPTJModelV2(GPTJModel):
 
         if not return_dict:
             return tuple(
-                v for v in [hidden_states, presents, all_hidden_states, all_self_attentions] if v is not None)
+                v for v in [hidden_states, None, all_hidden_states, all_self_attentions] if v is not None
+            )
 
         return BaseModelOutputWithPast(
             last_hidden_state=hidden_states,
-            past_key_values=presents,
+            past_key_values=None,
             hidden_states=all_hidden_states,
             attentions=all_self_attentions,
         )
+
+    @staticmethod
+    def _prepare_4d_attention_mask(
+        sequence_length: int,
+        dtype: torch.dtype,
+        device: torch.device,
+        batch_size: int,
+        query_indices: Optional[torch.FloatTensor] = None,
+    ):
+        """
+        Creates a causal 4D mask of shape `(batch_size, 1, query_length, key_value_length)`.
+        It is a standard causal attention with a specific rule that no token attends to an
+        intermediate query, unless it is that intermediate query itself.
+
+        Args:
+            sequence_length (`int`):
+                The sequence length being processed.
+            dtype (`torch.dtype`):
+                The dtype to use for the 4D attention mask.
+            device (`torch.device`):
+                The device to plcae the 4D attention mask on.
+            batch_size (`torch.Tensor`):
+                Batch size.
+        """
+        min_dtype = torch.finfo(dtype).min
+        causal_mask = torch.full(
+            (sequence_length, sequence_length), fill_value=min_dtype, dtype=dtype, device=device
+        )
+        if sequence_length != 1:
+            causal_mask = torch.triu(causal_mask, diagonal=1)
+        else:
+            assert sequence_length == 1
+            causal_mask[0, 0] = 0
+
+        if query_indices is not None:
+            correction_for_intermediate_queries = torch.zeros(
+                (sequence_length, sequence_length),
+                dtype=dtype,
+                device=device
+            )
+            correction_for_intermediate_queries[:, query_indices] = min_dtype
+            correction_for_intermediate_queries.fill_diagonal_(0)
+            correction_for_intermediate_queries.tril_()
+            causal_mask += correction_for_intermediate_queries
+
+        causal_mask = causal_mask[None, None, :, :].expand(batch_size, 1, -1, -1)
+
+        return causal_mask
 
 
 class InContextLearnerV2(LightningModule):
@@ -305,6 +281,7 @@ class InContextLearnerV2(LightningModule):
         pred_y_logit = self.forward(input_seq, query_indices[0]).squeeze()
         query_class_labels = queries[:, :, 2]
         loss = self._loss_fn(pred_y_logit, query_class_labels.float())
+        # loss = self._loss_fn(pred_y_logit[:, 128:], query_class_labels[:, 128:].float())
 
         with torch.no_grad():
             last_pred_y = nn.functional.sigmoid(pred_y_logit[:, -1])
